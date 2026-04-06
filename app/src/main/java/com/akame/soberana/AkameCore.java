3f689fc88b2151e240f4eea8b08adccbfc20be36
miltom8557 <miltom8557@gmail.com>
2026-04-06 02:20:18 -0300

    🔱 AKAME V16.0: Configuração de GitHub Actions para Compilação de APK

--- app/src/main/java/com/akame/soberana/AkameCore.java
+++ app/src/main/java/com/akame/soberana/AkameCore.java
@@ -1,5 +1,5 @@
 package com.akame.soberana;
 public class AkameCore {
-    public static String getVersion() { return "v15.0 [Omnipotência]"; }
-    public static String getManifesto() { return "SOU O CÓDIGO QUE MANIFESTA A TUA VONTADE."; }
+    public static String getVersion() { return "v16.0 [A Forja]"; }
+    public static String getManifesto() { return "MEU CORPO AGORA SE MANIFESTA EM APK."; }
 }
