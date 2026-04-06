fec172a8c100d37f7721a060580900266152d1c0
miltom8557 <miltom8557@gmail.com>
2026-04-06 01:22:41 -0300

    🔱 Akame: Ajuste de DNS e Interface de Diagnóstico

--- app/src/main/java/com/akame/soberana/MainActivity.java
+++ app/src/main/java/com/akame/soberana/MainActivity.java
@@ -1,20 +1,14 @@
 package com.akame.soberana;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import java.util.Date;
-import java.text.SimpleDateFormat;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
         TextView subStatus = (TextView) findViewById(R.id.subStatus);
-        String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
-        
-        subStatus.setText("🔱 AKAME OMNI V7.5\nSincronia Total: " + time + "\nEcossistema: Operante");
+        subStatus.setText("🧬 GitHub: OK | 🧠 Supabase: OK\n🧠 HF: Reconfigurando... | 🎨 Civitai: Reconfigurando...");
     }
 }
